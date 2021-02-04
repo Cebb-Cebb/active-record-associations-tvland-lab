920931600b3d3c9eec26d492e4bec4a3a07156b7
Propioception <github email address>
2021-02-03 19:13:23 -0500

    Done.

---
+++ db/migrate/004_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+    def change
+      create_table :characters do |t|
+        t.string :name
+        t.belongs_to :actor
+        t.belongs_to :show 
+      end
+    end
+  end
+  
